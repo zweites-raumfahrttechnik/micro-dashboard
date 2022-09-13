cd7274afd10f477762179d435dc0ebea21eb69b3
Jinquan Wang <w1637894214@163.com>
2022-09-13 22:22:54 +0800

    ✨ feat: 添加一个持久化插件

--- src/model/index.ts
+++ src/model/index.ts
@@ -3,8 +3,10 @@ import { createPinia } from 'pinia';
 
 import { useAppModel } from './modules/app';
 import { useTabModel } from './modules/tab';
+import { StoragePlugin } from './plugin';
 
 const pinia = createPinia();
+pinia.use(StoragePlugin);
 
 function bootstrapPinia(app: App) {
   app.use(pinia);
