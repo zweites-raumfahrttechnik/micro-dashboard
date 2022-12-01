9c446706505f1af8105bd9b71f676f7bfbc3af7b
ChenXuguang <943603518@qq.com>
2022-12-01 18:31:17 +0800

    server proxy

--- vite.config.ts
+++ vite.config.ts
@@ -17,6 +17,15 @@ export default defineConfig({
     ...createVuePlugin(),
     ...createAutoImportPlugin(),
     ...createArcoPlugin(),
-    ...createMockPlugin(),
+    //...createMockPlugin(),
   ],
+  server: {
+    proxy: {
+      '/api/user': {
+        target: 'http://127.0.0.1:8081',
+        rewrite: path => path.replace(/^\/api\/user/, ''),
+      },
+      '/api': { target: 'http://127.0.0.1:8080', rewrite: path => path.replace(/^\/api/, '') },
+    },
+  },
 });
